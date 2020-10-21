b78378befdd9a1802886e5908a9d5efc7aed3bc0
YoungJoo <skeedc@hotmail.com>
2020-10-21 23:14:43 +0900

    1st commit..

---
+++ src/main/java/rentalService/Product.java
@@ -0,0 +1,67 @@
+package rentalService;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name="Product_table")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    @ColumnDefault("10") //default 10
+    private int qty ;
+    
+    @PrePersist
+    public void onPrePersist(){
+        /* 서킷브레이크 테스트  */
+
+        try {
+            Thread.currentThread().sleep((long) (800 + Math.random() * 220));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @PostPersist
+    public void onPostPersist(){
+        ProductSaved productSaved = new ProductSaved();
+        BeanUtils.copyProperties(this, productSaved);
+        productSaved.publishAfterCommit();
+        
+        // 무중단 배포 테스트     
+
+    }
+
+    /*
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+    */
+}
